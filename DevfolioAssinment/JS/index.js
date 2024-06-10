6c5f5e724e0ddc85b048524b1a6b3b6b43b63c04
unknown <mohamedmadyan31@gmail.com>
2024-06-10 11:10:02 +0300

    first commit

---
+++ DevfolioAssinment/JS/index.js
@@ -0,0 +1,18 @@
+var userName="5"
+console.log(typeof userName)
+
+
+var userAge=20
+console.log(typeof userAge)
+
+
+
+var empolyee= true
+console.log(typeof empolyee)
+
+var x ;
+console.log(typeof x)
+
+
+var salary = null 
+console.log(typeof salary);
